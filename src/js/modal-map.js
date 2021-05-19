a454da7e921723c1291f719d345c9a7d92deb0d7
OksiDavydova <oksana.acva@gmail.com>
2021-05-19 23:42:31 +0300

    restart

--- src/js/modal-map.js
+++ src/js/modal-map.js
@@ -1,15 +1,41 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+// ;(() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[modal-map-open]'),
+//     closeModalBtn: document.querySelector('[modal-map-close]'),
+//     modal: document.querySelector('[modal-map]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+//   function toggleModal() {
+//     document.body.classList.toggle("modal-open");
+//     refs.modal.classList.toggle('is-hidden-cont');
+//   }
+// })();
+
+// (() => {
+//   const mobileMenu = document.querySelector('.js-menu-container');
+//   const openMenuBtn = document.querySelector('.js-open-menu');
+//   const closeMenuBtn = document.querySelector('.js-close-menu');
+
+//   const toggleMenu = () => {
+//     const isMenuOpen =
+//       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//     document.body.classList.toggle("modal-open");
+//     mobileMenu.classList.toggle('is-open');
+//   };
+
+//   openMenuBtn.addEventListener('click', toggleMenu);
+//   closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   // Закрываем мобильное меню на более широких экранах
+//   // в случае изменения ориентации устройства.
+//   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//     if (!e.matches) return;
+//     mobileMenu.classList.remove('is-open');
+//     openMenuBtn.setAttribute('aria-expanded', false);
+//   });
+// })();
+//
