b4d465cc74ebeabbbc4093ab7e5f942a8db7a342
OksiDavydova <oksana.acva@gmail.com>
2021-05-20 11:43:54 +0300

    changde script

--- src/js/anchor-scroll.js
+++ src/js/anchor-scroll.js
@@ -1,36 +1,13 @@
-// // скрипт плавной прокрутки страницы до якоря
-// let $root = $('html, body');
-// $('a[href^="#"]').click(function () {
-//   let href = $.attr(this, 'href');
-//   $root.animate({
-//     scrollTop: $(href).offset().top
-//   }, 1000, function () {
-//     window.location.hash = href;
-//   });
-//   return false;
-// });
-
 $(document).ready(function () {
-  // Add smooth scrolling to all links
   $("a").on('click', function (event) {
-
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
-
-      // Store hash
       var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 1000, function () {
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
-    } // End if
+    } 
   });
 });
